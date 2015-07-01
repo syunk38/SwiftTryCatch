e06147abe6baa4ee4b3a95323e89ab977477a31b
kuroki <shuntaro.kuroki@livesense.co.jp>
2015-07-01 21:07:38 +0900

    save

--- swift2.0OverView/ViewController.swift
+++ swift2.0OverView/ViewController.swift
@@ -10,9 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var label: UILabel!
+    
+    enum ParseError: ErrorType {
+        case Nil // nil だった場合
+        case IllegalFormat(String) // Int としてパースできなかった場合
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        textView.text.removeAll()
+        do {
+            textView.text = try self.getTextFromLavel()
+        }
+        catch ParseError.Nil{
+            
+        }
+        
+    }
+    
+    func getTextFromLavel() throws -> String? {
+        do {
+            let text = try label.text
+        }
+        catch ParseError.Nil {
+            print("the text is nill")
+            throw ParseError.Nil
+        }
+        catch ParseError.IllegalFormat(let text) {
+            print("format is \(text)")
+            throw ParseError.IllegalFormat(text)
+        }
+        catch {
+            print("unexpected error")
+        }
+        return text
     }
 
     override func didReceiveMemoryWarning() {
